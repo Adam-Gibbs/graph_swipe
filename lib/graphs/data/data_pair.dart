d1554dcc40e57e0782af66367b1f53a34780688f
Adam Gibbs <adam-gibbs@outlook.com>
2021-05-16 23:52:40 +0100

    Remove unecessary variables setting in data pair

--- lib/graphs/data/data_pair.dart
+++ lib/graphs/data/data_pair.dart
@@ -1,15 +1,12 @@
 import 'package:graph_swipe/graphs/data/data.dart';
 
 class DataPair extends Data {
-  late double value;
   late double min;
   late double max;
 
   DataPair(List<double> inList) : super((inList[0] + inList[1]) / 2) {
     this.min = inList[0];
     this.max = inList[1];
-    // TODO: is this needed?
-    this.value = (inList[0] + inList[1]) / 2;
   }
 
   List<double> getvalues() {
